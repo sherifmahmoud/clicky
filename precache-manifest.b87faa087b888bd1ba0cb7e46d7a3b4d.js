6fe240689c0b028d474757b67bbf0cf327f87654
Sherif Mahmoud <sherif.h.mahmoud@gmail.com>
2018-10-08 12:12:09 -0500

    Updates

---
+++ precache-manifest.b87faa087b888bd1ba0cb7e46d7a3b4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e53dddbe16ca2509119",
+    "url": "/clicky/static/js/runtime~main.9e53dddb.js"
+  },
+  {
+    "revision": "2a9e6719c25595430aa0",
+    "url": "/clicky/static/js/main.2a9e6719.chunk.js"
+  },
+  {
+    "revision": "7eae6140ad0adf38fbdc",
+    "url": "/clicky/static/js/1.7eae6140.chunk.js"
+  },
+  {
+    "revision": "2a9e6719c25595430aa0",
+    "url": "/clicky/static/css/main.9b48f5d8.chunk.css"
+  },
+  {
+    "revision": "3264e9e5f1a37a6a736b4401cf5ae7ef",
+    "url": "/clicky/index.html"
+  }
+];
